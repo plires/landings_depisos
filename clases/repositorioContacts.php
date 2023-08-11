b5d419b64e2562da94de374f2bef9ff6c273c119
Pablo Lires <pablolires@gmail.com>
2023-08-11 14:33:18 -0300

    Agregado de funcion para asignar el mismo vendedor a usuarios que ya consultaron

--- clases/repositorioContacts.php
+++ clases/repositorioContacts.php
@@ -3,7 +3,8 @@
 abstract class repositorioContacts {
 	public abstract function saveInBDD($post);
 	public abstract function getSalesEmails($rubro);
-	public abstract function getNextEmailTo($rubro);
+	public abstract function getSellerForThisUser($rubro, $userFound);
+	public abstract function userFound($email_user);
 }
 
 ?>
