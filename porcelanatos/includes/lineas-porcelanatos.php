2ea35f6dae106b6a3eaa65ac4156f07b07514ecf
Pablo Lires <pablolires@gmail.com>
2023-11-29 13:15:32 -0300

    adecuacion landing porcelanatos vite

---
+++ porcelanatos/includes/lineas-porcelanatos.php
@@ -0,0 +1,33 @@
+<!-- Líneas -->
+<section class="container lineas">
+
+  <div class="row">
+    <div class="col-md-12 wow fadeInUp">
+      <h2>NUESTRAS <span>LÍNEAS</span></h2>
+    </div>
+  </div>
+
+  <div class="row wow fadeInUp">
+    <div class="col-md-6">
+      <img class="img-fluid" src="./img/linea-vite.jpg" alt="linea porcelanatos vite">
+      <h3>PORCELANATOS VITE</h3>
+    </div>
+
+    <div class="col-md-6">
+      <img class="img-fluid" src="./img/linea-tendenza.jpg" alt="linea porcelanatos tendenza">
+      <h3>PRCELÁNICOS TENDENZA</h3>
+    </div>
+    <div class="col-md-6">
+      <img class="img-fluid" src="./img/linea-cerro-negro.jpg" alt="linea porcelanatos cerro negro">
+      <h3>PORCELANATOS CERRO NEGRO</h3>
+    </div>
+    <div class="col-md-6">
+      <img class="img-fluid" src="./img/linea-importados.jpg" alt="linea porcelanatos importados">
+      <h3>PORCELANATOS IMPORTADOS</h3>
+    </div>
+  </div>
+
+  <a href="#formulario" class="btn btn-primary transition btn_to_form">CONSULTÁ OTRAS MARCAS DISPONIBLES</a>
+
+</section>
+<!-- Líneas end -->
--- porcelanatos/vite.php
+++ porcelanatos/vite.php
@@ -162,36 +162,7 @@
   <!-- Faja Vite end -->
 
   <!-- Líneas -->
-  <section class="container lineas">
-
-    <div class="row">
-      <div class="col-md-12 wow fadeInUp">
-        <h2>NUESTRAS <span>LÍNEAS</span></h2>
-      </div>
-    </div>
-
-    <div class="row wow fadeInUp">
-      <div class="col-md-6">
-        <img class="img-fluid" src="img/linea-vite.jpg" alt="linea porcelanatos vite">
-        <h3>VITE</h3>
-      </div>
-
-      <div class="col-md-6">
-        <img class="img-fluid" src="img/linea-tendenza.jpg" alt="linea porcelanatos tendenza">
-        <h3>PRCELÁNICOS TENDENZA</h3>
-      </div>
-      <div class="col-md-6">
-        <img class="img-fluid" src="img/linea-cerro-negro.jpg" alt="linea porcelanatos cerro negro">
-        <h3>CERRO NEGRO</h3>
-      </div>
-      <div class="col-md-6">
-        <img class="img-fluid" src="img/linea-importados.jpg" alt="linea porcelanatos importados">
-        <h3>IMPORTADOS</h3>
-      </div>
-    </div>
-
-  </section>
-  <!-- Líneas end -->
+  <?php include('./includes/lineas-porcelanatos.php'); ?>
 
   <!-- Faja Naranja -->
   <section class="container-fluid faja_naranja">
