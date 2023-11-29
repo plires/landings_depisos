a8331f9a82778e5bd1e6112166787804d80a4664
Pablo Lires <pablolires@gmail.com>
2023-11-29 13:22:27 -0300

    edicion de page gracias de porcelanatos

--- porcelanatos/gracias.php
+++ porcelanatos/gracias.php
@@ -1,81 +1,94 @@
 <!DOCTYPE html>
 <html lang="es">
-	<head>
-		<!-- Required meta tags -->
-		<meta charset="utf-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-		<meta name="description" content="Gracias por contactarse. Pisos porcelanatos. Colores naturales. Las mejores marcas y el mejor precio del mercado">
-		<meta name="author" content="Librecomunicacion">
-	  <!-- Favicons -->
-	  <?php include('./../includes/favicon.inc.php'); ?>
 
-		<title>Gracias - Depisos.com. todo para tus pisos</title>
-		<link rel="stylesheet" href="./css/app.css">
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description"
+    content="Gracias por contactarse. Pisos porcelanatos. Colores naturales. Las mejores marcas y el mejor precio del mercado">
+  <meta name="author" content="Librecomunicacion">
+  <!-- Favicons -->
+  <?php include('./../includes/favicon.inc.php'); ?>
 
-		<?php include('./../includes/tag_manager_head.php') ?>
-	</head>
-	<body class="gracias">
-		<?php include('./../includes/tag_manager_body.php') ?>
+  <title>Gracias - Depisos.com. todo para tus pisos</title>
+  <link rel="stylesheet" href="./css/app.css">
 
-		<!-- Header -->
-		<?php include_once( __DIR__ . '/../includes/header.php' );  ?>
+  <?php include('./../includes/tag_manager_head.php') ?>
+</head>
 
-		<!-- Imagen Destacada -->
-		<section class="container-fluid imagen_destacada">
+<body class="gracias">
+  <?php include('./../includes/tag_manager_body.php') ?>
 
-			<!-- Mejores Marcas -->
-			<div class="mejores_marcas wow fadeInRight">
-				<img class="img-fluid" src="img/marcas.png" alt="mejores marcas pisos a prueba de agua">
-				<div>
-					<p>las <span>mejores</span> <br> marcas del <br> mercado</p>
-				</div>
-			</div>
-			<!-- Mejores Marcas end -->
+  <!-- Header -->
+  <?php include_once( __DIR__ . '/../includes/header.php' );  ?>
 
-			<!-- Informacion -->
-			<div class="container">
-				<div class="row">
+  <!-- Imagen Destacada -->
+  <section class="container-fluid imagen_destacada">
 
-					<div class="col-md-12">
-						<h1 class="wow fadeInDown">ENVIO EXITOSO!</h1>
-						<p class="wow fadeInLeft cuotas">Te contestaremos lo antes posible.</p>
-						<a href="https://depisos.com/" class="btn btn-primary">VER MAS PRODUCTOS</a>
-					</div>
+    <!-- Mejores Marcas -->
+    <div class="mejores_marcas wow fadeInRight">
+      <img class="img-fluid" src="img/marcas.png" alt="mejores marcas pisos a prueba de agua">
+      <div>
+        <p>las <span>mejores</span> <br> marcas del <br> mercado</p>
+      </div>
+    </div>
+    <!-- Mejores Marcas end -->
 
-				</div>
-			</div>
-			<!-- Informacion end -->
+    <!-- Informacion -->
+    <div class="container">
+      <div class="row">
 
-		</section>
-		<!-- Imagen Destacada -->
+        <div class="col-md-12">
+          <h1 class="wow fadeInDown">ENVIO EXITOSO!</h1>
+          <p class="wow fadeInLeft cuotas">Te contestaremos lo antes posible.</p>
+          <a href="https://depisos.com/" class="btn btn-primary">VER MAS PRODUCTOS</a>
+        </div>
 
-		<!-- Líneas -->
-		<section class="container lineas">
-			
-			<div class="row">
-				<div class="col-md-12 wow fadeInUp">
-					<h2>NUESTRAS <span>LÍNEAS</span></h2>
-				</div>
-			</div>
+      </div>
+    </div>
+    <!-- Informacion end -->
 
-			<div class="row wow fadeInUp">
-				<div class="col-md-6">
-					<img class="img-fluid" src="img/linea-cerro-negro.jpg" alt="linea porcelanatos cerro negro">
-					<h3>CERRO NEGRO</h3>
-				</div>
-				<div class="col-md-6">
-					<img class="img-fluid" src="img/linea-importados.jpg" alt="linea porcelanatos importados">
-					<h3>IMPORTADOS</h3>
-				</div>
-			</div>
+  </section>
+  <!-- Imagen Destacada -->
 
-		</section>
-		<!-- Líneas end -->
+  <!-- Líneas -->
+  <section class="container lineas">
 
-		<!-- Footer -->
-		<?php include_once( __DIR__ . '/../includes/footer.php' );  ?>
+    <div class="row">
+      <div class="col-md-12 wow fadeInUp">
+        <h2>NUESTRAS <span>LÍNEAS</span></h2>
+      </div>
+    </div>
 
-		<script src="./../dist/main.js"></script>
+    <div class="row wow fadeInUp">
+      <div class="col-md-6">
+        <img class="img-fluid" src="./img/linea-vite.jpg" alt="linea porcelanatos vite">
+        <h3>PORCELANATOS VITE</h3>
+      </div>
 
-	</body>
+      <div class="col-md-6">
+        <img class="img-fluid" src="./img/linea-tendenza.jpg" alt="linea porcelanatos tendenza">
+        <h3>PRCELÁNICOS TENDENZA</h3>
+      </div>
+      <div class="col-md-6">
+        <img class="img-fluid" src="./img/linea-cerro-negro.jpg" alt="linea porcelanatos cerro negro">
+        <h3>PORCELANATOS CERRO NEGRO</h3>
+      </div>
+      <div class="col-md-6">
+        <img class="img-fluid" src="./img/linea-importados.jpg" alt="linea porcelanatos importados">
+        <h3>PORCELANATOS IMPORTADOS</h3>
+      </div>
+    </div>
+
+  </section>
+  <!-- Líneas end -->
+
+  <!-- Footer -->
+  <?php include_once( __DIR__ . '/../includes/footer.php' );  ?>
+
+  <script src="./../dist/main.js"></script>
+
+</body>
+
 </html>
